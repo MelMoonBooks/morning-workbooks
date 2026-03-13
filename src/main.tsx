e236b9d99fcefe4bc1bdd4b4ab229884e50696ea
MelMoonBooks <beckwithmelanie@gmail.com>
2026-03-13 16:22:21 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ClerkProvider } from '@clerk/clerk-react'
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-react'
 import './index.css'
 import App from './App.tsx'
+import LandingPage from './LandingPage.tsx'
 
 const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!publishableKey) {
-  throw new Error('Missing Publishable Key')
-}
+if (!publishableKey) { throw new Error('Missing Publishable Key') }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={publishableKey}>
-      <App />
+      <SignedOut>
+        <LandingPage />
+      </SignedOut>
+      <SignedIn>
+        <App />
+      </SignedIn>
     </ClerkProvider>
   </StrictMode>,
 )
